d32b4da87646c7816f78dc7f25a31247ee67ca01
jyothika <jyothikavempatapu8778@gmail.com>
2021-02-19 11:14:17 +0530

    Initial commit

---
+++ about.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int x,y;
+printf("ener x , y values");
+scanf("%d%d",&x,&y);
+if(x>y)
+{
+printf("x s greater ");
+}
+else
+{
+printf("y is reater");
+}
+getch();
+}
